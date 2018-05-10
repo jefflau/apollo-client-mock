e8b1435b0900e81ee1805eda45a61e39187c4753
Jeff Lau <jeff-lau@live.com>
2018-05-10 16:17:51 +0800

    Convert to cjs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { ApolloClient } = require('apollo-client')
 const { InMemoryCache } = require('apollo-cache-inmemory')
 const { SchemaLink } = require('apollo-link-schema')
-const { makeExecutableSchema, addMockFunctionsToSchema } = require('graphql-tools')
-import merge from require('lodash/merge')
+const {
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} = require('graphql-tools')
+const merge = require('lodash/merge')
 
 function setupClient(mockResolvers, typeDefs) {
   return function createClient(overwriteMocks = {}) {
